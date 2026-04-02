42be7ed9231cff4ad3bfd888f9c0d926d9089982
DeepSea-badminton <deepsea123bad@gmail.com>
2026-04-02 19:21:04 +0900

    history.js を更新

--- deepsea/history/history.js
+++ deepsea/history/history.js
@@ -4,23 +4,26 @@
 // ============================
 const historyData = [
   // 2025年
-  { date: "2025-04-13", priority: 1, title: "大会用の名前だけのチームとして発足" },
-  { date: "2025-04-13", priority: 3, title: "武蔵村山オープン MD3部🥉" },
-  { date: "2025-04-13", priority: 2, title: "1stユニフォーム作成" },
-  { date: "2025-04-17", priority: 1, title: "募集サイトでチームメンバー募集開始" },
-  { date: "2025-04-22", priority: 1, title: "LINEグループ作成" },
-  { date: "2025-05-03", priority: 3, title: "YTKCUP MD2🥈" },
-  { date: "2025-08-31", priority: 1, title: "NHK「24時間テレビ」出演" },
-  { date: "2025-09-15", priority: 3, title: "小平市民シングルス大会 MS2🥉・MS3🥇🥈" },
-  { date: "2025-09-29", priority: 3, title: "北本オープン団体戦 男子Aチーム🥇" },
-  { date: "2025-10-05", priority: 3, title: "小平市民混合ダブルス大会にて代表ACL断裂" },
-  { date: "2025-11-16", priority: 3, title: "小平市民ダブルス大会 MD3🥇（代表半月板損傷）" },
-  { date: "2025-12-16", priority: 3, title: "代表 ACL・半月板 手術" },
+ { date: "2025-04-13", priority: 1, title: "大会用の名前だけのチームとして発足" }
+ ,{ date: "2025-04-13", priority: 3, title: "武蔵村山オープン MD3部🥉" }
+ ,{ date: "2025-04-13", priority: 2, title: "1stユニフォーム作成" }
+ ,{ date: "2025-04-17", priority: 1, title: "募集サイトでチームメンバー募集開始" }
+ ,{ date: "2025-04-22", priority: 1, title: "LINEグループ作成" }
+ ,{ date: "2025-05-03", priority: 3, title: "YTKCUP MD2🥈" }
+ ,{ date: "2025-08-31", priority: 1, title: "NHK「24時間テレビ」出演" }
+ ,{ date: "2025-09-15", priority: 3, title: "小平市民シングルス大会 MS2🥉・MS3🥇🥈" }
+ ,{ date: "2025-09-29", priority: 3, title: "北本オープン団体戦 男子Aチーム🥇" }
+ ,{ date: "2025-10-05", priority: 3, title: "小平市民混合ダブルス大会にて代表ACL断裂" }
+ ,{ date: "2025-11-16", priority: 3, title: "小平市民ダブルス大会 MD3🥇（代表半月板損傷）" }
+ ,{ date: "2025-12-16", priority: 3, title: "代表 ACL・半月板 手術" }
 
   // 2026年
-  { date: "2026-01-18", priority: 3, title: "調布市民ダブルス大会 MD3🥈" },
-  { date:　"2026-01-28",　priority: 2,　title: "当ホームページ開設" },
-  { date:　"2026-01-28",　priority: 3,　title: "小平市民総合体育館で団体利用登録完了" }
+ ,{ date: "2026-01-18", priority: 3, title: "調布市民ダブルス大会 MD3🥈" }
+ ,{ date:　"2026-01-28",　priority: 2,　title: "当ホームページ開設" }
+ ,{ date:　"2026-01-28",　priority: 3,　title: "小平市民総合体育館で団体利用登録完了" }
+ ,{ date:　"2026-02-18",　priority: 3,　title: "小平元気村おがわ東で団体利用登録完了" }
+ ,{ date:　"2026-02-21",　priority: 3,　title: "小平市民総合体育館で団体利用登録完了" }
+,{ date: "2026-03-27", priority: 3, title: "北区高校生ダブルス大会 WD2🥉" }
 ];
 
 // ============================
